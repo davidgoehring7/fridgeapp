e0b5524b64cfb689dbc4957e7d3083fd36fa1ab7
david.goehring7 <david.goehring7@gmail.com>
2018-02-13 00:30:36 -0500

    Initial Checkin

---
+++ FridgeInventory/ClientApp/app/components/inventory/inventory.component.ts
@@ -0,0 +1,24 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'inventory',
+    templateUrl: './inventory.component.html'
+})
+export class InventoryComponent {
+    columnDefs;
+    rowData;
+
+    constructor() {
+        this.columnDefs = [
+            { headerName: "Make", field: "make", width: 300 },
+            { headerName: "Model", field: "model", width: 300 },
+            { headerName: "Price", field: "price", width: 300 }
+        ];
+
+        this.rowData = [
+            { make: "Toyota", model: "Cecelia", price: 35000},
+            { make: "Ford", model: "Monaco", price: 31200 },
+            { make: "Porsche", model: "Boxer", price: 23440 }
+        ];
+    }
+}
